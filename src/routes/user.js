9572e1ebbf24fd6b253ddca2848414bdbbd0ffad
TrigalJeruti <tjesquivel@uc.cl>
2022-05-30 01:46:07 -0400

    Probando

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,1 +1,13 @@
+const Router = require('express');
+const router = Router();
 
+
+router.get('/', async(req, res) => {
+    try{
+        await res.send({"hi" : "bye"});
+    }catch (error){
+        console.log("error");
+    }
+});
+
+module.exports = router;
