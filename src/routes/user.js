286eb2261f63ef4ec97b7592cac0ff313a2a288c
TrigalJeruti <tjesquivel@uc.cl>
2022-05-30 02:11:13 -0400

    Probando segundo endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,22 @@
+require('dotenv').config();
+const njwt = require('njwt');
 const Router = require('express');
 const router = Router();
 const user = require('../models').user;
+const usertoken = require('../models').usertoken;
+const jwtgenerator = require('jsonwebtoken');
+const jwt = require('express-jwt');
 
-router.get('/si', async(req, res) => {
-    try{
-        await res.send({"hi" : "bye"});
-    }catch (error){
-        console.log("error");
-    }
-});
 
+function generateToken(user) {
+    return new Promise((resolve, reject) => {
+        jwtgenerator.sign(
+            {sub: user.id},
+            process.env.JWT_SECRET,
+            (err, tokenResult) => (err ? reject(err) : resolve(tokenResult)),
+        );
+    });
+};
 
 /* ------------------------------------------------------------ */
 
@@ -31,6 +38,60 @@ router.get('/', async(req, res) => {
     }
 });
 
+
+/* ------------------------------------------------------------ */
+
+router.post('/', async(req, res) => {
+    try{
+        const { username, password, name, age, psu_score, university, gpa_score, job, salary, promotion, hospital, operations, medical_debt } = req.body;
+
+        if (
+            typeof(username) !== 'string' ||
+            typeof(password) !== 'string' ||
+            typeof(name) !== 'string' ||
+            typeof(age) !== 'number' ||
+            age.toString().includes('.') ||
+            typeof(psu_score) !== 'number' ||
+            psu_score.toString().includes('.') ||
+            typeof(university) !== 'string' ||
+            typeof(gpa_score) !== 'number' ||
+            typeof(job) !== 'string' ||
+            typeof(salary) !== 'number' ||
+            typeof(promotion) !== 'boolean' ||
+            typeof(hospital)  !== 'string' ||
+            typeof(operations)  !== 'object' ||
+            Array.isArray(operations) !== true ||
+            typeof(medical_debt) !== 'number'
+        ){
+            return res
+                .status(400)
+                .json({"error": "invalid attributes"});
+        }
+
+        const exist_u = await user.findOne({ where: {'username': username} });
+        const exist_p = await user.findOne({ where: {'password': password} });
+
+        if (exist_u || exist_p) {
+            return res
+                .status(409)
+                .json({"error": "user already exists"});
+        }
+    
+        const usuario = await user.create({ username,password,name,age,psu_score,university,gpa_score,job,salary,promotion,hospital,operations,medical_debt });
+        const token = await generateToken(usuario);
+        await usertoken.create({ "userid": usuario.id, "token": token});
+
+        res
+            .status(201)
+            .json({
+                "id": usuario.id,
+                "toke": token
+            });
+
+    }catch (error){
+        throw new Error(error);
+    }
+});
 
 
 module.exports = router;
