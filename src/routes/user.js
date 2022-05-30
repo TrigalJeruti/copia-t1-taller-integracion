6c9d6a30d6757979e741159a0042aef668418cc8
TrigalJeruti <tjesquivel@uc.cl>
2022-05-30 01:51:08 -0400

    probando solo endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,12 +2,35 @@ const Router = require('express');
 const router = Router();
 
 
-router.get('/', async(req, res) => {
+router.get('/si', async(req, res) => {
     try{
         await res.send({"hi" : "bye"});
     }catch (error){
         console.log("error");
     }
 });
+
+
+/* ------------------------------------------------------------ */
+
+router.get('/', async(req, res) => {
+    try{
+        const hola = ['true', 'hola'];
+        console.log(typeof(hola));
+        const hola_ = hola.toString();
+        if (typeof(hola) == 'number' && hola.toString().includes('.')){
+            console.log("Holiwiss");
+        }
+        const usuarios = await user.findAll();
+        res.json({ usuarios }).status(200);
+        if (!user) {
+            return res.json({ error: 'User not found' }).status(404);
+        }
+    }catch (error){
+        throw new Error(error);
+    }
+});
+
+
 
 module.exports = router;
